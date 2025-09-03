835fc8ca02c301a1d54a3af01ad01e89088fb47a
fcoterroba <fcoterroba@gmail.com>
2025-09-03 09:25:03 +0200

    feat(rust kata): return negative

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # codewars
+
 repo to save answers of the daily codewars kata i do
+
+## index
+
+- wednesday -> [Rust](https://github.com/fcoterroba/codewars/tree/main/Rust/)
---
+++ rust/return_negative/mysolution.rs
@@ -0,0 +1,6 @@
+fn make_negative(n: i32) -> i32 {
+    if n > 0 { -n } else { n }
+}
+
+// original kata: https://www.codewars.com/kata/55685cd7ad70877c23000102
+// my solution: https://www.codewars.com/kata/reviews/65ad920589c7f40001f524ef/groups/65b271738f45d800017497f6
