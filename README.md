23feb842a2d396060fd53ee5a0f487b78465f746
fcoterroba <fcoterroba@gmail.com>
2025-09-08 08:21:32 +0200

    feat(cpp kata): school paperwork 1

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # codewars
+
 repo to save the daily answers of my codewars katas
+
+## index of content
+
+- [monday of c++](https://github.com/fcoterroba/codewars/tree/main/cpp/)
---
+++ cpp/schoolpaperwork/mysolution.cpp
@@ -0,0 +1,9 @@
+int paperwork(int n, int m){
+    if(n < 0 || m < 0){
+        return 0;
+    }
+        return n*m;
+}
+
+// original kata: https://www.codewars.com/kata/55f9b48403f6b87a7c0000bd
+// my solution: https://www.codewars.com/kata/reviews/600432c4f6c8f20001be921f/groups/600432c4f6c8f20001be9223
