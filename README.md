cbb6f045adcd4f1651dd08680e9b22ef2d90f4fa
fcoterroba <fcoterroba@gmail.com>
2025-10-13 12:38:58 +0200

    feat(cpp kata): boolean to string

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # codewars
 repo to save all my daily codewars' kata answers
+
+## index of content
+
+- monday of c++: https://github.com/fcoterroba/codewars/tree/main/cpp
---
+++ cpp/boolean_to_string/mysolution.cpp
@@ -0,0 +1,9 @@
+#include <string>
+
+std::string bool_to_word(bool value)
+{
+  return value ? "Yes" : "No";
+}
+
+// original kata: https://www.codewars.com/kata/53369039d7ab3ac506000467
+// my solution: https://www.codewars.com/kata/reviews/575fb0db77f0495393000053/groups/5763f4451d8b62b8510000ec
