4b19cd82297792f24995003160d1e815633a410d
fcoterroba <fcoterroba@gmail.com>
2025-10-20 07:52:44 +0200

    feat(cpp kata): string to number

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # codewars
+
 repo to save all my daily kata's codewars answers
+
+## index of content
+
+- monday of c++: https://github.com/fcoterroba/CodeWars/tree/main/cpp
---
+++ cpp/stringtonumber/mysolution.cpp
@@ -0,0 +1,8 @@
+#include <string>
+
+int string_to_number(const std::string& s) {
+  return std::stoi(s);
+}
+
+// original kata: https://www.codewars.com/kata/544675c6f971f7399a000e79
+// my solution: https://www.codewars.com/kata/reviews/58058bced0070d471b00001d/groups/58061a969c8751de320000c7
