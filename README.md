6308f28f6aaad917ebeabc6beb392c1e6e74fcb0
fcoterroba <fcoterroba@gmail.com>
2025-10-21 06:58:46 +0200

    feat(cs kata): code under pressure 1

--- README.md
+++ README.md
@@ -4,4 +4,5 @@ repo to save all my daily kata's codewars answers
 
 ## index of content
 
-- monday of c++: https://github.com/fcoterroba/CodeWars/tree/main/cpp
+- monday of c++: https://github.com/fcoterroba/codewars/tree/main/cpp
+- tuesday of c#: https://github.com/fcoterroba/codewars/tree/main/cs
---
+++ cs/codeunderpressure1/mysolution.cs
@@ -0,0 +1,12 @@
+using System;
+
+public static class Kata
+{
+  public static int DoubleInteger(int n)
+  {
+    return n*2;
+  }
+}
+
+// original kata: https://www.codewars.com/kata/53ee5429ba190077850011d4
+// my solution: https://www.codewars.com/kata/reviews/5990b31135fd2f187600000e/groups/599172e9fab78caad300062d
