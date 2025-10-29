26580d127ae8786d5b9ac8cb46a79cc9672beb09
fcoterroba <fcoterroba@gmail.com>
2025-10-29 08:21:37 +0100

    feat(rs kata): grasshoper summation

---
+++ rs/grasshopersummation/mysolution.rs
@@ -0,0 +1,6 @@
+fn summation(n: i32) -> i32 {
+    (1..=n).sum()
+}
+
+// original kata: https://www.codewars.com/kata/55d24f55d7dd296eb9000030
+// my solution: https://www.codewars.com/kata/reviews/5f7c2b3f7d74a90001e79f23/groups/5f7dfc6500336d0001a514ca
