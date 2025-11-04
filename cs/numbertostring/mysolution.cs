fe5c5eca6715282986c6be51ec526eb3ecf8e27f
fcoterroba <fcoterroba@gmail.com>
2025-11-04 07:48:35 +0100

    feat(cs kata): number to string

---
+++ cs/numbertostring/mysolution.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Kata
+{
+  public static string NumberToString(int num)
+  {
+    return num.ToString();
+  }
+}
+
+// original kata: https://www.codewars.com/kata/5265326f5fda8eb1160004c8
+// my solution: https://www.codewars.com/kata/reviews/5991a8b253fd57e3b60000d9/groups/5991ad17e947d63e2b002c7e
