a20ac5b224e524c698c180e81feaea413a0bc486
fcoterroba <fcoterroba@gmail.com>
2025-11-13 22:04:20 +0100

    feat(php kata): array diff

---
+++ php/arraydiff/mysolution.php
@@ -0,0 +1,9 @@
+<?php
+
+function arrayDiff($a, $b)
+{
+    return array_values(array_diff($a, $b));
+}
+
+// original kata: https://www.codewars.com/kata/523f5d21c841566fde000009
+// my solution: https://www.codewars.com/kata/reviews/5bacdc96484fcdf5f4002054/groups/5baceb24484fcd767f00003f
