281a41e6acefa43a5909ec114ecf04c9fd0c1076
fcoterroba <46996764+fcoterroba@users.noreply.github.com>
2025-11-18 06:56:20 +0100

    feat(cs kata): training on function 1

---
+++ cs/trainingonfunction1/mysolution.cs
@@ -0,0 +1,9 @@
+public static class Kata
+{
+  public static string greet() {
+      return "hello world!";
+  }
+}
+
+// original kata: https://www.codewars.com/kata/523b4ff7adca849afe000035
+// my solution: https://www.codewars.com/kata/reviews/5518bd8c4854f4c77b000019/groups/5c929d6ff4672d00019e0437
