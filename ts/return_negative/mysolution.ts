0ae36b4a8f61276b1c3a8499f513640e9aca94fb
fcoterroba <fcoterroba@gmail.com>
2025-12-01 08:31:57 +0100

    feat(ts kata): return negative

---
+++ ts/return_negative/mysolution.ts
@@ -0,0 +1,6 @@
+export const makeNegative = (num: number): number => {
+    return num < 0 ? num : -num;
+};
+
+// original kata: https://www.codewars.com/kata/55685cd7ad70877c23000102
+// my solution: https://www.codewars.com/kata/reviews/5a11a18958b744668800072e/groups/5a607461400f936428000216
