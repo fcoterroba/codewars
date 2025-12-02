121de1288707002869f766d1fde3b74365dac9e6
fcoterroba <fcoterroba@gmail.com>
2025-12-02 08:44:51 +0100

    feat(cs kata): cockroach

---
+++ cs/cockroach/mysolution.cs
@@ -0,0 +1,9 @@
+using System;
+public class Cockroach {
+    public static int CockroachSpeed(double x) {
+        return (int)(x * 100000 / 3600);
+    }
+}
+
+// original kata: https://www.codewars.com/kata/55fab1ffda3e2e44f00000c6/train/csharp
+// my solution: https://www.codewars.com/kata/reviews/55fab53096625f04d7000158/groups/59cc88ee018903083b001454
