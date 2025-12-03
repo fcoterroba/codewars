865e56fa279b4d808c4ea22327d263cd770c1afd
fcoterroba <fcoterroba@gmail.com>
2025-12-03 08:48:43 +0100

    feat(rs kata): plural

---
+++ rs/plural/mysolution.rs
@@ -0,0 +1,6 @@
+fn plural (n: f64) -> bool {
+    return n != 1.0;
+}
+
+// original kata: https://www.codewars.com/kata/52ceafd1f235ce81aa00073a
+// my solution: https://www.codewars.com/kata/reviews/626ac20f902ebf0001f3d0d1/groups/626aee88902ebf0001f3db8a
