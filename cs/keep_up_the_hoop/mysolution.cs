736f9ad6d8c2cecbccfc4d37e6e2ab6c7cef1845
fcoterroba <fcoterroba@gmail.com>
2025-12-10 15:35:35 +0100

    feat(cs kata): keep up the hoop

---
+++ cs/keep_up_the_hoop/mysolution.cs
@@ -0,0 +1,10 @@
+public class Kata
+{
+  public static string HoopCount(int n)
+  {
+    return n <= 10 ? "A" : "B";
+  }
+}
+
+// original kata: https://www.codewars.com/kata/55cb632c1a5d7b3ad0000145
+// my solution: https://www.codewars.com/kata/reviews/55d197956077327afa000051/groups/55d1d86fd1eeb12f0600010f
