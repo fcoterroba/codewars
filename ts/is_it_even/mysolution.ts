4bdbaee0a2a5d2e0fbcb080bc3676555e2771d80
fcoterroba <fcoterroba@gmail.com>
2025-12-15 08:42:59 +0100

    feat(ts kata): is it even

---
+++ ts/is_it_even/mysolution.ts
@@ -0,0 +1,6 @@
+export const testEven = (n : number) : boolean => {
+  return n % 2 == 0 ? true : false
+}
+
+// original kata: https://www.codewars.com/kata/555a67db74814aa4ee0001b5
+// my solution: https://www.codewars.com/kata/reviews/61732a20be622b000181e75e/groups/62251290bb23e50001a3e15e
