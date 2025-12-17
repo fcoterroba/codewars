d2aaf14480dc9302b996a3ce3c674d060e5d9332
fcoterroba <fcoterroba@gmail.com>
2025-12-17 09:23:27 +0100

    feat(rs kata): check for factor

---
+++ rs/check_for_factor/mysolution.rs
@@ -0,0 +1,6 @@
+fn check_for_factor(base: i32, factor: i32) -> bool {
+    base % factor == 0
+}
+
+// original kata: https://www.codewars.com/kata/55cbc3586671f6aa070000fb
+// my solution: https://www.codewars.com/kata/reviews/5e3ea94e76126b0001d59e91/groups/5e3ea94f90bbd20001f09dc6
