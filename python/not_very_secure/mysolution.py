f070e4f4689b3d0ebe18415088d71840da34d6e5
fcoterroba <fcoterroba@gmail.com>
2025-12-19 11:46:17 +0100

    feat(python kata): not very secure

---
+++ python/not_very_secure/mysolution.py
@@ -0,0 +1,5 @@
+def alphanumeric(password: str) -> bool:
+    return password.isalnum()
+
+# original kata: https://www.codewars.com/kata/526dbd6c8c0eb53254000110
+# my solution: https://www.codewars.com/kata/reviews/552d66e999020732900002ae/groups/5d9b7652e1cd9d0001e0e12a
