7b98adcb84ae4a407e6be65c68293ae3dcaf47ea
fcoterroba <fcoterroba@gmail.com>
2025-12-24 09:16:38 +0100

    feat(rs kata): square sum

---
+++ rs/square_sum/mysolution.rs
@@ -0,0 +1,6 @@
+fn square_sum(vec: Vec<i32>) -> i32 {
+    vec.iter().map(|x| x * x).sum()
+}
+
+// original kata: https://www.codewars.com/kata/515e271a311df0350d00000f
+// my solution: https://www.codewars.com/kata/reviews/5824a8aa304383a19900005a/groups/5826266dd0872180ba0006ff
