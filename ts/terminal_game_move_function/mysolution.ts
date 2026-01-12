4bef6a8e6077ed370c956d867e67f518ab137003
fcoterroba <fcoterroba@gmail.com>
2026-01-12 11:39:54 +0100

    feat(ts kata): terminal game move grasshoper

---
+++ ts/terminal_game_move_function/mysolution.ts
@@ -0,0 +1,6 @@
+export function move(pos: number, roll: number): number {
+    return roll+roll+pos
+}
+
+// original kata: https://www.codewars.com/kata/563a631f7cbbc236cf0000c2
+// my solution: https://www.codewars.com/kata/reviews/5d829e9515a9070001e422d3/groups/629a1a091774b90001c0acdd
