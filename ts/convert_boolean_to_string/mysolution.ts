0579ac19a49d4749499da424c9a1a530ef579a07
fcoterroba <fcoterroba@gmail.com>
2026-01-19 10:07:04 +0100

    feat(ts kata): convert boolean to string

---
+++ ts/convert_boolean_to_string/mysolution.ts
@@ -0,0 +1,6 @@
+export const booleanToString = (b:boolean):string => {
+  return b ? "true" : "false"
+};
+
+// original kata: https://www.codewars.com/kata/551b4501ac0447318f0009cd
+// my solution: https://www.codewars.com/kata/reviews/5b5d981200ed5f9010000aed/groups/5b658fd96fd2cad34a00069d
