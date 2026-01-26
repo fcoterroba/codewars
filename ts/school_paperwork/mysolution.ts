e9fe93e2bd8925b642b6a3d37bcc238dbc01ced0
fcoterroba <fcoterroba@gmail.com>
2026-01-26 10:58:51 +0100

    feat(ts kata): school paperwork

---
+++ ts/school_paperwork/mysolution.ts
@@ -0,0 +1,7 @@
+export function paperwork(n: number, m :number): number{
+  if(n < 0 || m < 0) return 0;
+  return n * m;
+}
+
+// original kata: https://www.codewars.com/kata/55f9b48403f6b87a7c0000bd
+// my solution: https://www.codewars.com/kata/reviews/617192322363740001eec089/groups/61758e20271f0b0001916226
