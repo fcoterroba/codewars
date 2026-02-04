d5ac2db9799971c591f4a3cc4a8d7aabf77700fe
fcoterroba <fcoterroba@gmail.com>
2026-02-04 10:33:24 +0100

    feat(go kata): xor logical operator

---
+++ go/xor_logical_operator/mysolution.go
@@ -0,0 +1,11 @@
+package kata
+
+func Xor(a bool, b bool) bool {
+	if a == b {
+		return false
+	}
+	return true
+}
+
+// original kata: https://www.codewars.com/kata/56fa3c5ce4d45d2a52001b3c
+// my solution: https://www.codewars.com/kata/reviews/5e3da8f026937f0001873863/groups/6232f6aa8d843d0001b891d9
