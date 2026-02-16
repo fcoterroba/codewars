02943280027345707ecf57e60c2285944089edf9
fcoterroba <fcoterroba@gmail.com>
2026-02-16 10:24:34 +0100

    feat(ts kata): squaring an argument

---
+++ ts/squaring_an_argument/mysolution.ts
@@ -0,0 +1,4 @@
+export const square = (n: number): number => n * n;
+
+// original kata: https://www.codewars.com/kata/523b623152af8a30c6000027
+// my solution: https://www.codewars.com/kata/reviews/6166e8e1c0da6700015e42ea/groups/61672ff3c662a30001d40c8a
