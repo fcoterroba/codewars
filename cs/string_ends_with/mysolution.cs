81f94a4b1c84becad49fc2da73655104f34289e6
fcoterroba <fcoterroba@gmail.com>
2026-02-17 08:56:46 +0100

    feat(cs kata): string ends with

---
+++ cs/string_ends_with/mysolution.cs
@@ -0,0 +1,10 @@
+public class Kata
+{
+  public static bool Solution(string str, string ending)
+  {
+    return str.EndsWith(ending);
+  }
+}
+
+// original kata: https://www.codewars.com/kata/51f2d1cafc9c0f745c00037d
+// my solution: https://www.codewars.com/kata/reviews/5990b47735fd2f187600011c/groups/5990be1e800bb6fb300000a8
