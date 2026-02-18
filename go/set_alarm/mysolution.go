0888674d7a6dbd2dc9e5d4e7ba1ef3b8c602f26e
fcoterroba <fcoterroba@gmail.com>
2026-02-18 09:02:27 +0100

    feat(go kata): set alarm

---
+++ go/set_alarm/mysolution.go
@@ -0,0 +1,9 @@
+package kata
+
+
+func SetAlarm(employed, vacation bool) bool {
+  return employed && !vacation
+}
+
+// original kata: https://www.codewars.com/kata/568dcc3c7f12767a62000038/
+// my solution: https://www.codewars.com/kata/reviews/62c0f1a44989dd00011b01db/groups/62c0f1a44989dd00011b01df
