2164bf781f6b36d93dc31a9d5b7b84d95bea53db
fcoterroba <46996764+fcoterroba@users.noreply.github.com>
2026-02-24 08:48:55 +0100

    feat(cs kata): training on sum arrays

---
+++ cs/training_on_sum_arrays/mysolution.cs
@@ -0,0 +1,12 @@
+using System.Linq;
+
+public class Kata
+{
+  public static double SumArray(double[] array)
+  {
+    return array.Sum();
+  }
+}
+
+// original kata: https://www.codewars.com/kata/53dc54212259ed3d4f00071c
+// my solution: https://www.codewars.com/kata/reviews/5996efdc0b2b36bd930009e3/groups/5996f545e06bbfc13a001791
