c51cb5030e4deb4c046e05f072da953ce2e49612
fcoterroba <46996764+fcoterroba@users.noreply.github.com>
2026-02-27 08:55:46 +0100

    hotfix(so many permutations python kata): add links

--- python/so_many_permutations/mysolution.py
+++ python/so_many_permutations/mysolution.py
@@ -2,3 +2,6 @@ from itertools import permutations as _permutations
 
 def permutations(s):
     return sorted(set(''.join(p) for p in _permutations(s)))
+
+// original kata: https://www.codewars.com/kata/5254ca2719453dcc0b00027d
+// my solution: https://www.codewars.com/kata/reviews/555acc3335d4c449750001b5/groups/69a14dc745ea47da82621196
