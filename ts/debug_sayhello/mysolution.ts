d6f5f824bc050e487d456f02f26db1aa107d64e7
fcoterroba <46996764+fcoterroba@users.noreply.github.com>
2026-03-02 09:48:37 +0100

    feat(ts kata): debug say hello

---
+++ ts/debug_sayhello/mysolution.ts
@@ -0,0 +1,6 @@
+export function sayHello(name: string): string {
+  return 'Hello, ' + name
+}
+
+// original kata: https://www.codewars.com/kata/5625618b1fe21ab49f00001f
+// my solution: https://www.codewars.com/kata/reviews/5f2ae025e568cf0001faa73b/groups/5f2ae4d44123cf000174f305
