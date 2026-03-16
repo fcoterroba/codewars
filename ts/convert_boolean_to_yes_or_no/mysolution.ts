db0cc2eb7d489eee5ad46305692b7d1f21508e0c
fcoterroba <46996764+fcoterroba@users.noreply.github.com>
2026-03-16 09:05:59 +0100

    feat(ts kata): convert boolean to yes or no

---
+++ ts/convert_boolean_to_yes_or_no/mysolution.ts
@@ -0,0 +1,6 @@
+export const boolToWord = (bool: boolean): string => {
+  return bool ? "Yes" : "No"
+};
+
+// original kata: https://www.codewars.com/kata/53369039d7ab3ac506000467
+// my solution: https://www.codewars.com/kata/reviews/5aaffb063c0781a66500058a/groups/5ab103e6d8144c4519000dc2
