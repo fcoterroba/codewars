eb36c221059dac06e34eb814d9abb35c91a57ce0
fcoterroba <46996764+fcoterroba@users.noreply.github.com>
2026-03-30 10:33:12 +0200

    feat(ts kata): get character from ascii value

---
+++ ts/get_character_from_ascii_value/mysolution.ts
@@ -0,0 +1,6 @@
+export function getChar(c: number): string {
+  return String.fromCharCode(c)
+}
+
+// original kata: https://www.codewars.com/kata/55ad04714f0b468e8200001c
+// my solution: https://www.codewars.com/kata/reviews/64a577e4f0675b000145322b/groups/64a577eef0675b0001453230
