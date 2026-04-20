668cd83dda6becf30ddfafc4b7f868155417b6ba
fcoterroba <46996764+fcoterroba@users.noreply.github.com>
2026-04-20 09:42:55 +0200

    feat(ts kata): is the string uppercase

---
+++ ts/is_the_string_uppercase/mysolution.ts
@@ -0,0 +1,6 @@
+export function isUpperCase(str: string): boolean {
+  return str === str.toUpperCase();
+}
+
+// original kata: https://www.codewars.com/kata/56cd44e1aa4ac7879200010b
+// my solution: https://www.codewars.com/kata/reviews/5d6753991631be00016b1e99/groups/61d49edfc61d7f0001a51eb7
