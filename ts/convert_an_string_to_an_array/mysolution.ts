e3cdd25850298448ffe9c609fe5de4adde553a86
fcoterroba <46996764+fcoterroba@users.noreply.github.com>
2026-05-04 15:09:21 +0200

    feat(ts kata): convert an array to string

---
+++ ts/convert_an_string_to_an_array/mysolution.ts
@@ -0,0 +1,6 @@
+export function stringToArray(s: string): string[] {
+  return s.split(" ");
+}
+
+// original kata: https://www.codewars.com/kata/57e76bc428d6fbc2d500036d
+// my solution: https://www.codewars.com/kata/reviews/5ad107ecf83505bdc9000b48/groups/5ad1172410df4e542c00079d
