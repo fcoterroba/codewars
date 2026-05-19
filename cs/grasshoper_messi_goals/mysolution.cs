72589e061fc15bf035e51bcf7ca3c3e6420fe97f
fcoterroba <46996764+fcoterroba@users.noreply.github.com>
2026-05-19 09:14:27 +0200

    feat(cs kata): grasshoper messi goals

---
+++ cs/grasshoper_messi_goals/mysolution.cs
@@ -0,0 +1,12 @@
+using System;
+
+public static class Kata 
+{
+    public static int la_liga_goals = 43;
+    public static int champions_league_goals = 10;
+    public static int copa_del_rey_goals = 5;
+    public static int total_goals = copa_del_rey_goals + champions_league_goals + la_liga_goals;
+}
+
+// original kata: https://www.codewars.com/kata/55ca77fa094a2af31f00002a
+// my solution: https://www.codewars.com/kata/reviews/56f48800e419f1cad8000009/groups/58aab41d58737a81cc0016ff
