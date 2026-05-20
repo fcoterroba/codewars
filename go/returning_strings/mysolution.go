1351244180abbd14ca66223cf6e635f85d34de31
fcoterroba <46996764+fcoterroba@users.noreply.github.com>
2026-05-20 10:20:34 +0200

    feat(go kata): returning strings

---
+++ go/returning_strings/mysolution.go
@@ -0,0 +1,8 @@
+package kata
+
+func Greet(name string) string {
+  return "Hello, " + name + " how are you doing today?"
+}
+
+// original kata: https://www.codewars.com/kata/55a70521798b14d4750000a4
+// my solution: https://www.codewars.com/kata/reviews/5f9074b916083f000140c59d/groups/5f9074bb16083f000140c5a3
