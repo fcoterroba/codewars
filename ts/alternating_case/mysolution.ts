9c0ea5a494d0294f2532fef1aa0f01a182a24095
fcoterroba <46996764+fcoterroba@users.noreply.github.com>
2026-05-25 10:46:21 +0200

    feat(ts kata): alternating case

---
+++ ts/alternating_case/mysolution.ts
@@ -0,0 +1,6 @@
+export function toAlternatingCase(s: string): string {
+  return "someThingHere";
+}
+
+// original kata: https://www.codewars.com/kata/56efc695740d30f963000557
+// my solution: https://www.codewars.com/kata/reviews/5da92f95fe1fc50001d1f424/groups/6a140c4e7f4e1fbaa271e910
