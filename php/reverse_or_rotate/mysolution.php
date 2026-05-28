f72e1a914710667c9ca8c0a008021689dcc62aa0
fcoterroba <46996764+fcoterroba@users.noreply.github.com>
2026-05-28 12:26:54 +0200

    feat(php kata): reverse or rotate

---
+++ php/reverse_or_rotate/mysolution.php
@@ -0,0 +1,4 @@
+<?php
+
+// original kata: https://www.codewars.com/kata/56b5afb4ed1f6d5fb0000991
+// my solution: https://www.codewars.com/kata/reviews/579c65f63fb8d9634c00043a/groups/6a181857c06dcccc6d79caa8
