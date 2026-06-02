9e38594bd64ed32c9aadee0d75fdf91a3703745b
fcoterroba <46996764+fcoterroba@users.noreply.github.com>
2026-06-02 10:14:11 +0200

    feat(cs kata): how many lightsabers do you own

---
+++ cs/how_many_lightsabers_do_you_own/mysolution.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Kata
+{
+  public static int HowManyLightsabersDoYouOwn(string name)
+  {
+    return name == "Zach" ? 18 : 0;
+  }
+}
+
+// original kata: https://www.codewars.com/kata/51f9d93b4095e0a7200001b8
