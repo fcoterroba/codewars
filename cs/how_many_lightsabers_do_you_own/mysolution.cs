6bbf410d93277f6ed544b3c140a37215f36d5aba
fcoterroba <46996764+fcoterroba@users.noreply.github.com>
2026-06-02 10:14:49 +0200

    hotfix(cs kata): add my solution link comment

--- cs/how_many_lightsabers_do_you_own/mysolution.cs
+++ cs/how_many_lightsabers_do_you_own/mysolution.cs
@@ -9,3 +9,4 @@ public class Kata
 }
 
 // original kata: https://www.codewars.com/kata/51f9d93b4095e0a7200001b8
+// my solution: https://www.codewars.com/kata/reviews/55dc989cc504659e15000004/groups/55dc989fc504659e15000013
