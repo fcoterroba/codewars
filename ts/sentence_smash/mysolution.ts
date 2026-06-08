0dd74702b331b96bcdf1c4dbcb29678ef46001f3
fcoterroba <46996764+fcoterroba@users.noreply.github.com>
2026-06-08 08:25:06 +0200

    feat(ts kata): sentence smash

---
+++ ts/sentence_smash/mysolution.ts
@@ -0,0 +1,6 @@
+export function smash(words: string[]): string {
+    return words.join(" ");
+}
+
+// original kata: https://www.codewars.com/kata/53dc23c68a0c93699800041d
+// my solution: https://www.codewars.com/kata/reviews/61d17cb564c2350001383189/groups/61d17cb6383a50000129fd9a
