ddc07bb66c573f74ce37aa499ee734dad32ae9ed
fcoterroba <46996764+fcoterroba@users.noreply.github.com>
2026-08-03 16:55:21 +0200

    feat(ts kata): remove duplicates from list

---
+++ ts/remove_duplicates_from_list/mysolution.ts
@@ -0,0 +1,6 @@
+export const distinct = (a: number[]): number[] => {
+  return [...new Set(a)];
+}
+
+// original kata: https://www.codewars.com/kata/57a5b0dfcf1fa526bb000118
+// my solution: https://www.codewars.com/kata/reviews/6154394eced5470001aa0e07/groups/61543954ced5470001aa0e0d
