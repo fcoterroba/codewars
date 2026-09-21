84cbd3b4b8ac0dd5fe0868680d1280988eb159bf
fcoterroba <46996764+fcoterroba@users.noreply.github.com>
2026-09-21 10:54:27 +0200

    feat(ts kata): what is the real floor

---
+++ ts/what_the_real_floor/mysolution.ts
@@ -0,0 +1,4 @@
+export const getRealFloor = (n: number): number => n > 13 ? n - 2 : n > 0 ? n - 1 : n;
+
+// original kata: https://www.codewars.com/kata/574b3b1599d8f897470018f6
+// my solution: todo: pending to appear in the list
